b4b06d68e61bd3a7aa09f5b43dc0b28a4fa1557c
Aritra Basu <43692750+aritrakrbasu@users.noreply.github.com>
2021-02-13 10:33:37 +0530

    destruction 100

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-fetch('https://meme-api.herokuapp.com/gimme/100')
+const urlParams = new URLSearchParams(window.location.search);
+var spamcount = urlParams.get('spam');
+var memepage = urlParams.get('memepage');
+if(spamcount == null)
+{
+    spamcount = 100;
+}
+if(memepage == null)
+{
+    var memeurl = 'https://meme-api.herokuapp.com/gimme/'+spamcount;
+}else
+{
+    var memeurl = 'https://meme-api.herokuapp.com/gimme/'+memepage+'/'+spamcount;
+}
+fetch(memeurl)
 .then(function(response){
     return response.json();// convert it to a pure JavaScript object
 })
 .then(function(data){
 
     data.memes.forEach(meme => {
+        console.log(meme)
         var payload = {
             "name":meme.author,
             "caption":meme.title,
@@ -29,7 +44,9 @@ function spamdata(payload)
     })
     .then(response => response.json())
     .then(data => {
-        console.log("#spamed");
+        var div = document.createElement('div');
+            div.innerHTML = "Spamming Hambuuuuuuuuuuuuuuuuuuuu , New meme id = "+data.id+"<br>check here <a href='http://xmeme-soh.herokuapp.com/memes/"+data.id+"' target='_blank'}>Click here </a>";
+            document.body.appendChild(div);
         return true;
     })
     .catch((error) => {
